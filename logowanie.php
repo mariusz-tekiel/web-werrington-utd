3ce48ab6ee375a62b5e9f8e0c8f165a63318771d
MTekiel <mtekiel777@gmail.com>
2019-07-06 10:01:51 +0100

    Przed dodaniem kodu z zaloguj.php

--- logowanie.php
+++ logowanie.php
@@ -7,8 +7,16 @@
              header("Location: index-new.php?error=1");
              exit();
          }
+        
+         require_once"connect.php";
+	     
+        $polaczenie = new mysqli($host,$db_user,$db_password,$db_name);
+       /* $db = mysql_connect('localhost','root','') or die ("Database connection denied!") ;
+        mysql_select_db('programowanie');
+        $wynik = mysql_query("SELECT * FROM users WHERE login*$login") or die("Downloading data not successful!");
 
-
+        $rows = mysql_fetch_array($wynik);
+        echo $rows['login'];*/
 
     }else{
         header("Location: index-new.php?errror=0");
