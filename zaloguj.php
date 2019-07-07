50617a11121b8360e127f03ab515822323d9c356
MTekiel <mtekiel777@gmail.com>
2019-07-07 09:45:02 +0100

    Display user from database ok

--- zaloguj.php
+++ zaloguj.php
@@ -5,14 +5,30 @@
 		$polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
 		
 		if($polaczenie->connect_error!=0){
-			echo "Error: ".$polaczenie->connect_errno."Opis: ".$polaczenie->connect_error;
+			echo "Error: ".$polaczenie->connect_errno;
 		} 
 		else
 		{
 			$login = $_POST['login'];
 			$haslo = $_POST['haslo'];
 
-			echo "It works";
+			$sql = "SELECT * FROM users WHERE user='$login' AND pass='$haslo'";
+			// ten if sorawdza czy zapytanie zostalo poprawnie wykonane
+			if($rezultat = @$polaczenie->query($sql)){
+				$ilu_userow = $rezultat->num_rows;
+				if($ilu_userow>0){
+					$wiersz = $rezultat->fetch_assoc();
+					$user = $wiersz['user'];
+
+					$rezultat->free_result();
+					echo $user;
+				} else {
+
+				}
+			}
+
+
+			echo "<br/><br/>It works";
 			$polaczenie->close();
 
 		}
