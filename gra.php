b69538f71c4b07b573a959f9b0a9fabdd98a3ae9
MTekiel <mtekiel777@gmail.com>
2019-07-13 09:00:35 +0100

    tables displayed ok. Next step delete particular record

--- gra.php
+++ gra.php
@@ -16,6 +16,35 @@
 <body>
     
 <?php
+try {
+    $con= new PDO('mysql:host=localhost;dbname=werrington', 'root', '');
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "SELECT team_id as NO, team_name as TEAM, played as PLAYED, lost as LOST, won as WON, points as POINTS FROM `teams`";
+  //first pass just gets the column names
+    print '<table class="table" border="2px"> ';
+    $result = $con->query($query);
+  //return only the first row (we only need field names)
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+      print " <tr>";
+    foreach ($row as $field => $value){
+      print " <th>$field</th>";
+    } // end foreach
+      print " </tr>";
+    //second query gets the data
+      $data = $con->query($query);
+      $data->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($data as $row){
+        print " <tr>";
+      foreach ($row as $name=>$value){
+          print " <td>$value</td>";
+       } // end field loop
+      print " </tr>";
+    } // end record loop
+    print "</table>";
+ }
+ catch(PDOException $e) {
+   echo 'ERROR: ' . $e->getMessage();
+ } // end try
 		
 		echo "<p>Witaj ".$_SESSION['user'].'![<a href="logout.php">Logout</a>]</p>';
 
--- tableEditor.php
+++ tableEditor.php
@@ -15,18 +15,87 @@
 </head>
 
 <body>
-    
-<?php
-		
-		echo "<p>Witaj ".$_SESSION['user'].'![<a href="logout.php">Logout</a>]</p>';
+ <header>
+	<?php
 
+		echo '<a href="logout.php">Logout</a>';
+		echo "<p>Witaj ".$_SESSION['user'].'!</p>';
 		echo 'Jesteś zalogowany. Cool!<br><br>';
-		echo "<b>Drewno: </b> ".$_SESSION['drewno'];
-		echo "| <b>Kamien: </b> ".$_SESSION['kamien'];
-		echo "| <b>Zboze: </b> ".$_SESSION['zboze'];
-		echo "| <b>E-mail: </b> ".$_SESSION['email'];
-		echo "| <b>Dni premium: </b> ".$_SESSION['dnipremium'];
-?>
+	?>
 
+</header>
+<section class = "table">
+	<h2>TABLE TEAMS EDITOR</h2>
+	<br>
+
+	<?php
+	try {
+		$con= new PDO('mysql:host=localhost;dbname=werrington', 'root', '');
+		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$query = "SELECT team_id as NO, team_name as TEAM, played as PLAYED, lost as LOST, won as WON, points as POINTS FROM `teams`";
+	//first pass just gets the column names
+		print '<table class="table" border="2px"> ';
+		$result = $con->query($query);
+	//return only the first row (we only need field names)
+		$row = $result->fetch(PDO::FETCH_ASSOC);
+		print " <tr>";
+		foreach ($row as $field => $value){
+		print " <th>$field</th>";
+		} // end foreach
+		print " </tr>";
+		//second query gets the data
+		$data = $con->query($query);
+		$data->setFetchMode(PDO::FETCH_ASSOC);
+		foreach($data as $row){
+			print " <tr>";
+		foreach ($row as $name=>$value){
+			print " <td>$value</td>";
+		} // end field loop
+		print " </tr>";
+		} // end record loop
+		print "</table>";
+	}
+	catch(PDOException $e) {
+	echo 'ERROR: ' . $e->getMessage();
+	} // end try
+	
+	?>
+</section>
+
+<section class="table">
+	<h2>TABLE MATCHES EDITOR</h2>
+	<br>
+	<?php
+		try {
+			$con= new PDO('mysql:host=localhost;dbname=werrington', 'root', '');
+			$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$query = "SELECT match_id as ID, match_date as MATCH_DATE,team1_name as HOME_TEAM,team2_name as GUEST_TEAM,team1_score as HT_SCORE,team2_score as GT_SCORE FROM matches";
+		//first pass just gets the column names
+			print '<table class="table" border="2px"> ';
+			$result = $con->query($query);
+		//return only the first row (we only need field names)
+			$row = $result->fetch(PDO::FETCH_ASSOC);
+			print " <tr>";
+			foreach ($row as $field => $value){
+			print " <th>$field</th>";
+			} // end foreach
+			print " </tr>";
+			//second query gets the data
+			$data = $con->query($query);
+			$data->setFetchMode(PDO::FETCH_ASSOC);
+			foreach($data as $row){
+				print " <tr>";
+			foreach ($row as $name=>$value){
+				print " <td>$value</td>";
+			} // end field loop
+			print " </tr>";
+			} // end record loop
+			print "</table>";
+		}
+		catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+		} 		
+	?>
+</section>
 </body>
 </html>
