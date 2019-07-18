762b42fbc13fdc951e816c4b1b55de0753b773ca
MTekiel <mtekiel777@gmail.com>
2019-07-18 12:15:54 +0100

    Table 1 edition done. Next step table2 edit

--- setNumber.php
+++ setNumber.php
@@ -1,22 +1,26 @@
 <?php
-
 require_once 'database.php';
 	session_start();
 	if(!isset($_SESSION['zalogowany'])){
 		header('Location: indexLogin.php');
 		exit();
     }
-    
-    
-    echo $_POST['id'];
-    
-    
+       
+  
     if(isset($_POST['id'])){
         include_once("config.php");
-        $id=$_POST['id'];
+        $id = $_POST['id'];
+        $new_id = $_POST['new_id'];
+        
+        echo $id;
+        echo $new_id;
+
 		$conn = @new mysqli('localhost','root','','werrington');
-		$sqlQuery = "DELETE FROM teams WHERE team_id = '$id'";
+       
+		$sqlQuery = "UPDATE teams SET team_id='$new_id' WHERE team_id=$id";
 		$resultSet = mysqli_query($conn, $sqlQuery) or die("database error:". mysqli_error($conn));
+		
+		 header('Location: tableEditor.php'); 
+
     }
-	 header('Location: tableEditor.php'); 
 ?>
