d834ef1e0504017e6f9d07bdd5db0692bf840f5f
MTekiel <mtekiel777@gmail.com>
2019-09-27 12:58:57 +0100

    Last changes - web not working fine

--- index.php
+++ index.php
@@ -51,10 +51,10 @@
 
 <body>
   <main>
-  <!--[if lt IE 8 ]>
-        <p class="browserupgrade">You are using an  <strong>outdated</strong> browser.
-        Please <a href="http://browsehappy.com/"> upgrade your browser</a> to improve your
-        experience. </p>
+  <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser.
+        Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your
+        experience.</p>
     <![endif]-->
 
   <!-- Navbar section -->
