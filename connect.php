dac9e573d0aa50f2a8c93d37db8020843d85cbcb
MTekiel <mtekiel777@gmail.com>
2019-09-28 19:39:02 +0100

    Correct conn string for server

--- connect.php
+++ connect.php
@@ -1,10 +1,17 @@
 <?php	
-	$host = "localhost";
+	/*$host = "localhost";
 	$db_user = "root";
 	$db_password = "";
-	$db_name = "werrington";
-	/*$host = "10.16.16.17";
-	$db_user = "werr-8ec-u-240701";
-	$db_password = "mario71";
-	$db_name = "werr-8ec-u-240701";*/
+	$db_name = "werrington";*/
+
+	$host = "s8.linuxpl.com";
+	$db_user = "mmaximus_werr";
+	$db_password = "Mario$71";
+	$db_name = "mmaximus_werr";
+		
+		/*$host = "10.16.16.17";
+		$db_user = "werr-8ec-u-240701";
+		$db_password = "mario71";
+		$db_name = "werr-8ec-u-240701";*/
+
 ?>
