b35f3a5a581d5d8f6a85b6658b2702d097f0d334
MTekiel <mtekiel777@gmail.com>
2019-09-28 14:37:22 +0100

    Correct conn string 2

--- config.php
+++ config.php
@@ -5,10 +5,15 @@
       'password' => '',
       'database' => 'werrington'*/
       
-      'host' => '10.16.16.17',
+    /*  'host' => '10.16.16.17',
       'user' => 'werr-8ec-u-240701',
       'password' => 'mario71',
-      'database' => 'werr-8ec-u-240701' 
+      'database' => 'werr-8ec-u-240701'  */
+
+      'host' => 's8.linuxpl.com',
+      'user' => 'mmaximus_werr',
+      'password' => 'Mario$71',
+      'database' => 'mmaximus_werr' 
 
   ];
 
