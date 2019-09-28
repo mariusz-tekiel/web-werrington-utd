02850390ffda0a24cdb91270710b13e23f559f77
MTekiel <mtekiel777@gmail.com>
2019-09-28 14:28:46 +0100

    Connect to linuxpl server

--- connect.php
+++ connect.php
@@ -1,10 +1,15 @@
 <?php	
-	$host = "localhost";
+	/*$host = "localhost";
 	$db_user = "root";
 	$db_password = "";
-	$db_name = "werrington";
+	$db_name = "werrington";*/
+
 	/*$host = "10.16.16.17";
 	$db_user = "werr-8ec-u-240701";
 	$db_password = "mario71";
 	$db_name = "werr-8ec-u-240701";*/
+	$host = "s8.linuxpl.com";
+	$db_user = "mmaximus_werr";
+	$db_password = "Mario$71";
+	$db_name = "werr";
 ?>
