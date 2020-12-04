9d0dca0856b587220c2609793f08adbef72629e5
MTekiel <mtekiel777@gmail.com>
2020-12-04 16:08:18 +0000

    Change link for indexLogin

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
             <a class="nav-link" href="#contact">Contact</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="peterborough-volleyball-login">Log In</a>
+            <a class="nav-link" href="indexLogin">Log In</a>
           </li>
         </ul>
       </div>
